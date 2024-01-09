3f0d4cbe9f3595fddbcf33a98c7ce2634a3c8f0b
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2024-01-08 18:56:29 -0600

    flask aloiwhuiewhfuiwehfi

--- website/auth.py
+++ website/auth.py
@@ -1,22 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 auth = Blueprint("auth", __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
     return render_template("login.html", name="Brian.", email="Enter your email: ")
 
 
-@auth.route('/logout')
+@auth.route('/logout', methods=["GET", "POST"])
 def logout():
     return "<h1>Logout page</h1>"
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html", name="Name:", input="idk")
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password_repeat = request.form.get("password_repeat")
 
+        if len(first_name) <= 2:
+            flash("First name needs to be more than 2 letters.", category="error")
 
-@auth.route('/signout')
-def signout():
-    return "<h1>signout page</h1>"
+        elif len(last_name) <= 2:
+            flash("Last name needs to be more than 2 letters.", category="error")
+
+        elif len(password) <= 12:
+            flash("Password needs to have a minimum of 12 characters.", category="error")
+
+        elif password_repeat != password:
+            flash("Passwords do not match.", category="error")
+        else:
+            flash("Your account has been successfully created!", category="success")
+    return render_template("signup.html")
